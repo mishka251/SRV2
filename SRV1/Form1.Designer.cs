5dd095e670837a4ba47852478a8301fcf01a4983
Mikhail Belov <mbelov@os-company.ru>
2018-10-19 10:43:12 +0500

    init

---
+++ SRV1/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace SRV1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lbBuf = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbWork = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(528, 242);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Старт";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(434, 242);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Стоп";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lbBuf
+            // 
+            this.lbBuf.FormattingEnabled = true;
+            this.lbBuf.Location = new System.Drawing.Point(245, 30);
+            this.lbBuf.Name = "lbBuf";
+            this.lbBuf.Size = new System.Drawing.Size(149, 251);
+            this.lbBuf.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(251, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Очередь";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Обработка";
+            // 
+            // lbWork
+            // 
+            this.lbWork.FormattingEnabled = true;
+            this.lbWork.Location = new System.Drawing.Point(17, 59);
+            this.lbWork.Name = "lbWork";
+            this.lbWork.Size = new System.Drawing.Size(129, 199);
+            this.lbWork.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 292);
+            this.Controls.Add(this.lbWork);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbBuf);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ListBox lbBuf;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lbWork;
+    }
+}
+
---
+++ SRV1/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SRV1
+{
+    public partial class Form1 : Form
+    {
+
+        Mutex mutex;
+        Queue<int> queue;
+        bool work = true;
+
+        Semaphore semaphore;
+
+        public Form1()
+        {
+            InitializeComponent();
+            r = new Random();
+            t1 = new Thread(new ThreadStart(tr1));
+
+            mutex = new Mutex();
+            queue = new Queue<int>();
+
+            ThreadPool.GetMaxThreads(out nWorkerThreads, out nCompletionThreads);
+            label2.Text += " в " + nWorkerThreads + " потоков";
+            semaphore = new Semaphore(0, nWorkerThreads);
+        }
+        int nWorkerThreads, nCompletionThreads;
+
+        Random r;
+        int GetNum()
+        {
+            return r.Next(1, 9);
+        }
+
+        void tr1()
+        {
+            while (work)
+            {
+                int num = GetNum();
+
+                mutex.WaitOne();
+                queue.Enqueue(num);
+                int[] arr = new int[queue.Count];
+
+                lbBuf.Invoke(new Action(() => lbBuf.Items.Add(num.ToString())));
+                try
+                {
+                    semaphore.Release();
+                }
+                catch
+                {
+
+                }
+                mutex.ReleaseMutex();
+
+
+                Thread.Sleep(r.Next(10, 20));
+            }
+        }
+
+        void tr2(object state)
+        {
+            while (work)
+            {
+                semaphore.WaitOne();
+                mutex.WaitOne();
+                bool has_elem = queue.Count > 0;
+                int num = 0;
+                if (has_elem)
+                {
+                    num = queue.Dequeue();
+                    lbBuf.Invoke(new Action(() => lbBuf.Items.RemoveAt(0)));
+                }
+                mutex.ReleaseMutex();
+                string str = "Поток " + Thread.CurrentThread.ManagedThreadId + " обрабатывает " + num;
+                lbWork.Invoke(new Action(() => lbWork.Items.Add(str)));
+
+                
+                Thread.Sleep(r.Next(1500, 250000));
+                lbWork.Invoke(new Action(() => lbWork.Items.Remove(str)));
+               
+            }
+        }
+
+
+        Thread t1;
+
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            t1.Start();
+            
+            for (int i = 0; i < nWorkerThreads; i++)
+                ThreadPool.QueueUserWorkItem(tr2);
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            work = false;
+            Thread.Sleep(300);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            work = false;
+            Thread.Sleep(250);
+            Application.Exit();
+        }
+    }
+}
